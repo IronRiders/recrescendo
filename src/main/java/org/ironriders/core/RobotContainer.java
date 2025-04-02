67231b6dc54e3c3b186363a8b1fde79cb7c57efc
isabelle <lshields822@gmail.com>
2025-04-01 17:01:53 -0700

    launch and drive setup

--- src/main/java/org/ironriders/core/RobotContainer.java
+++ src/main/java/org/ironriders/core/RobotContainer.java
@@ -8,15 +8,20 @@ import org.ironriders.climber.ClimberCommands;
 import org.ironriders.climber.ClimberSubsystem;
 import org.ironriders.drive.DriveCommands;
 import org.ironriders.drive.DriveSubsystem;
+import org.ironriders.lib.Constants;
 import org.ironriders.manipulation.intake.IntakeCommands;
 import org.ironriders.manipulation.intake.IntakeSubsystem;
 import org.ironriders.manipulation.launcher.LauncherCommands;
 import org.ironriders.manipulation.launcher.LauncherSubsystem;
 import org.ironriders.manipulation.pivot.PivotCommands;
 import org.ironriders.manipulation.pivot.PivotSubsystem;
+import  org.ironriders.lib.Utils;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
 
@@ -35,12 +40,44 @@ public class RobotContainer {
 	public ClimberSubsystem climberSubsystem = new ClimberSubsystem();
 	public ClimberCommands climberCommands = climberSubsystem.getCommands();
 
+	private final CommandXboxController primaryController = new CommandXboxController(
+		Constants.Identifiers.CONTROLLER_PRIMARY_PORT);
+	private final CommandGenericHID secondaryController = new CommandJoystick(
+		Constants.Identifiers.CONTROLLER_SECONDARY_PORT);
+
+	public RobotCommands robotCommands = new RobotCommands(
+		driveCommands,
+		launcherCommands,
+		pivotCommands,
+		intakeCommands, 
+		climberCommands
+	);
+
+
+
+
+	private double inversionCoeff = 1;
+
 	public RobotContainer() {
 		configureBindings();
 	}
 
 	private void configureBindings() {
-		
+		// DRIVE CONTROLS
+		driveSubsystem.setDefaultCommand(
+				robotCommands.driveTeleop(
+						() -> Utils.controlCurve(
+								-primaryController.getLeftY()* driveSubsystem.ControlSpeedMultipler *driveSubsystem.getinversionStatus(),
+								Constants.Drive.TRANSLATION_CONTROL_EXPONENT,
+								Constants.Drive.TRANSLATION_CONTROL_DEADBAND),
+						() -> Utils.controlCurve(
+								-primaryController.getLeftX()* driveSubsystem.ControlSpeedMultipler *driveSubsystem.getinversionStatus(),
+								Constants.Drive.TRANSLATION_CONTROL_EXPONENT,
+								Constants.Drive.TRANSLATION_CONTROL_DEADBAND),
+						() -> Utils.controlCurve(
+								primaryController.getRightX()* driveSubsystem.ControlSpeedMultipler *driveSubsystem.getinversionStatus(),
+								Constants.Drive.ROTATION_CONTROL_EXPONENT,
+								Constants.Drive.ROTATION_CONTROL_DEADBAND)));
 	}
 
 	public Command getAutonomousCommand() {
--- src/main/java/org/ironriders/manipulation/launcher/LauncherSubsystem.java
+++ src/main/java/org/ironriders/manipulation/launcher/LauncherSubsystem.java
@@ -1,15 +1,43 @@
 package org.ironriders.manipulation.launcher;
 
+import org.ironriders.lib.Constants;
 import org.ironriders.lib.IronSubsystem;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class LauncherSubsystem extends IronSubsystem {
 
     public LauncherCommands commands = new LauncherCommands(this);
 
-    // init shit
+    private final SparkMax primaryMotor = new SparkMax(Constants.Identifiers.LAUNCHER_MOTOR_RIGHT, MotorType.kBrushless); // lead motor is the right one
+    private final SparkMax followerMotor = new SparkMax(Constants.Identifiers.LAUNCHER_MOTOR_LEFT, MotorType.kBrushless);
 
     public LauncherSubsystem() {
+        SparkMaxConfig primaryConfig = new SparkMaxConfig();
+        SparkMaxConfig followerConfig = new SparkMaxConfig();
 
+        primaryConfig
+                .idleMode(IdleMode.kCoast)
+                .smartCurrentLimit(Constants.Launcher.LAUNCHER_MOTOR_STALL_LIMIT)
+                .inverted(true);
+
+        followerConfig
+                .idleMode(IdleMode.kCoast)
+                .smartCurrentLimit(Constants.Launcher.LAUNCHER_MOTOR_STALL_LIMIT)
+                .follow(Constants.Identifiers.LAUNCHER_MOTOR_RIGHT, true);
+
+        primaryMotor.configure(primaryConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        followerMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    }
+
+    public void setMotor(double speed) {
+        primaryMotor.set(speed);
     }
 
     public LauncherCommands getCommands() {
